630b1275ad7bca9964f6d82b1bfcd6f2099841e4
joseph-hellerstein <joseph.hellerstein@gmail.com>
2023-03-19 17:00:12 -0700

    Initial version of Biosimulations Viewer

--- src/vbiosim.py
+++ src/vbiosim.py
@@ -1,9 +1,10 @@
 '''A viewer for information on Biosimulations'''
 
 import dash
-import dash_html_components as html
+from dash import html
 import plotly.graph_objects as go
 from dash import dcc
+import dash_bootstrap_components as dbc
 import plotly.express as px
 from dash.dependencies import Input, Output
 import requests
@@ -11,45 +12,51 @@ import requests
 API_URL = "https://api.biosimulations.org"
 PROJECT_URL = "%s/projects" % API_URL
 response = requests.get(PROJECT_URL)
-projects = response.json()
-import pdb; pdb.set_trace()
+project_descs = response.json()
 
+# Construct the list of projects
+project_dct = {d["id"]: d for d in project_descs}
+project_ids = list(project_dct.keys())
+project_dropdowns = [dict(label=v, value=v) for v in project_ids]
 
-app = dash.Dash()
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 application = app.server
 
-df = px.data.stocks()
+project_col = dbc.Col(dbc.Row([
+      html.H2("Project"),
+      dcc.Dropdown(id = 'dropdown1', options = project_dropdowns,
+            value = project_ids[0]),
+      ])
+)
+details_col = dbc.Col(dbc.Row([
+      html.H2("Details"),
+      dcc.Markdown(id = 'details', style={'whiteSpace': 'pre-line'})
+      ])
+)
+
+app.layout = html.Div([
+    dbc.Row([
+          project_col,
+          details_col
+          ],
+    ),
+])
 
 
-app.layout = html.Div(id = 'parent', children = [
-    html.H1(id = 'H1', children = 'Styling using html components', style = {'textAlign':'center',\
-                                            'marginTop':40,'marginBottom':40}),
-
-        dcc.Dropdown( id = 'dropdown',
-        options = [
-            {'label':'GGoogle', 'value':'GOOG' },
-            {'label': 'AApple', 'value':'AAPL'},
-            {'label': 'AAmazon', 'value':'AMZN'},
-            ],
-        value = 'GOOG'),
-        dcc.Graph(id = 'bar_plot')
-    ])
-
-
-@app.callback(Output(component_id='bar_plot', component_property= 'figure'),
-              [Input(component_id='dropdown', component_property= 'value')])
+######### CALL BACKS ############
+@app.callback(Output(component_id='selection1', component_property= 'children'),
+              [Input(component_id='dropdown1', component_property= 'value')])
 def graph_update(dropdown_value):
-    print(dropdown_value)
-    fig = go.Figure([go.Scatter(x = df['date'], y = df['{}'.format(dropdown_value)],\
-                     line = dict(color = 'firebrick', width = 4))
-                     ])
+    return dropdown_value
 
-    fig.update_layout(title = 'Stock prices over time',
-                      xaxis_title = 'Dates',
-                      yaxis_title = 'Prices'
-                      )
-    return fig
+@app.callback(Output(component_id='details', component_property= 'children'),
+              [Input(component_id='dropdown1', component_property= 'value')])
+def getProjectDetails(project_id):
+    lst = ["**%s**: %s" % (k, v) for k, v in project_dct[project_id].items()]
+    return "\n".join(lst)
 
+########## SAVE #########
+#      style = {'textAlign':'center', 'marginTop':40,'marginBottom':40})
 
 
 if __name__ == '__main__':
